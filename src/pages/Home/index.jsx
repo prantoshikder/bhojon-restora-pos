d574117c278d5b37893892cad134108e12cc7b97
Pranto Shikder <48595837+prantoshikder@users.noreply.github.com>
2021-12-21 15:48:47 -0800

    Header & sidebar component added

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Header from '../../components/partials/Header';
+import Sidebar from '../../components/partials/Sidebar';
 
 const Home = () => {
-  return <div>From Home</div>;
+  return (
+    <div>
+      <Header />
+      <Sidebar />
+    </div>
+  );
 };
 
 export default Home;
